a84398773f3a72bce65b69d3e0d3a0b2d4a3ab83
Erick Medina <erickmedinabe@gmail.com>
2020-02-05 10:37:50 +0100

    added weight and id. Stuck on abilities and reading about loops

--- index.php
+++ index.php
@@ -1,8 +1,42 @@
 <?php
+// getting API
+$_GET['pokemon'];
 
-$poke_data = file_get_contents('https://pokeapi.co/api/v2/pokemon/'); // to get API file
-$result = json_decode($poke_data, true); // 'true' converts objects into associative arrays
-var_dump($result);
+$poke_url = 'https://pokeapi.co/api/v2/pokemon/' . $_GET['pokemon']; // Pokemon API url + search
+$poke_json = file_get_contents($poke_url); // to get data from API
+$result_array = json_decode($poke_json , true); // 'true' converts objects into associative arrays
+//var_dump($result_array);
+
+// variable for name
+$poke_name = $result_array['name'];
+var_dump($poke_name);
+
+// ID variable
+$poke_id = $result_array['id'];
+var_dump($poke_id);
+
+// weight variable
+$poke_weight = $result_array['weight'];
+var_dump($poke_weight);
+
+/* abilities variable
+
+$abilities_newArray = [];
+for ($x = 0; $x <= 2; $x++) {
+    $pokeAbilities = $abilities_newArray.push($result_array['abilities'][$x]['ability']['name']);
+    var_dump($pokeAbilities);
+}
+*/
+
+$array_abilities = array();
+for ($x = 0; $x < count(); $x++) {
+$array_abi = array_push($array_abilities, $result_array['abilities'][$x]['ability']['name']);
+var_dump($array_abi);
+
+
+
+}
+
 
 ?>
 <!doctype html>
@@ -16,7 +50,7 @@ var_dump($result);
 </head>
 <body>
 <form name="form" action="" method="get">
-    <input type="text" name="subject" id="subject" value="Type a pokemon name">
+    <input type="text" name="pokemon" id="poke-input" placeholder="Type your favorite pokemon">
     <button type="submit" name="">Click here</button>
 </form>
 </body>
